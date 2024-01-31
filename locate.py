0381437cb3c69cd3069d04c5d3a98fcb42451e0c
Gustavo Nunes <gustavompn06@gmail.com>
2024-01-31 08:18:36 -0300

    Inicio de correção do processo dpk

--- locate.py
+++ locate.py
@@ -3,4 +3,3 @@ import time
 
 time.sleep(1)
 print(pg.position())
-# (x=989, y=430) (pegar preço nome grande)  
--- test.py
+++ test.py
@@ -8,7 +8,24 @@ pg.FAILSAFE = True
 produtos = [['ph2960', 'sp271'], ['wo146'], ['sk421', 'ph2966']]  
 codigo = str(produtos[0][0])
 
-string = pyperclip.paste()
-test = string.split('-')[0]
-pyperclip.copy(test)
-pg.hotkey('ctrl', 'v')
+#dpk pesquisa
+alttab()
+pg.hotkey('ctrl', 't')
+abrir_site('https://www.kdapeca.com.br/login')
+time.sleep(5)
+pg.doubleClick(x=458, y=496)
+time.sleep(1)
+pg.press('esc')
+time.sleep(0.5)
+pg.click(x=301, y=251)
+digitar(codigo)
+pg.press('enter')
+time.sleep(2)
+try:
+    quantidade = pg.locateOnScreen(r'imagens/enquantonao_kdapeca.png')
+except pg.ImageNotFoundException:
+    pyperclip.copy('Indisponível')
+else:
+    pg.doubleClick(x=669, y=504)
+    pg.hotkey('ctrl', 'c')
+    unformat()  
