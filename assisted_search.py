ab176743a9c4c9354f401c2953a1ba0c94bcbba6
Gustavo Nunes <gustavompn06@gmail.com>
2024-03-11 14:04:27 -0300

    markup on second run

--- assisted_search.py
+++ assisted_search.py
@@ -261,6 +261,7 @@ else:
     pg.write('+40')
     pg.press('enter')
 
+#markup
 pg.doubleClick(x=56, y=181)
 pg.write('$C$2:$G$2')
 pg.press('enter')
@@ -517,6 +518,47 @@ for peça in produtos:
                     pg.write('+40')
                     pg.press('enter')
 
+                #markup
+                pg.doubleClick(x=56, y=181)
+                pg.write(f'$C${celula_excel}:$G${celula_excel}')
+                pg.press('enter')
+                pg.hotkey('ctrl', 'c')
+
+                teste = pyperclip.paste()
+
+                num = ''
+                num_list = []
+                final_list = []
+                for c in teste:
+                    if c == ',':
+                        num += '.'
+                    elif c == '\t' or c == '\n':
+                        if c == teste[0]:
+                            continue
+                        else:
+                            num_list.append(num)
+                            num = ''
+                    elif c == '\r':
+                        continue
+                    else:
+                        num += c
+
+                for i, v in enumerate(num_list):
+                    if v != 'Indisponível':
+                        final_list.append(float(v))
+
+                try:
+                    celula = f'{(sum(final_list) / len(final_list)) * 1.8:.2f}'
+                except ZeroDivisionError:
+                    pass
+                else:
+                    pg.doubleClick(x=56, y=181)
+                    pg.write('h' + str(celula_excel))
+                    pg.press('enter')
+                    pyperclip.copy(celula)
+                    pg.hotkey('ctrl', 'v')
+                pg.press('esc')
+
                 #indice increase
                 celula_peca = 5
             else:
@@ -723,6 +765,47 @@ for peça in produtos:
                     pg.hotkey('ctrl', 'v')
                     pg.write('+40')
                     pg.press('enter')
+
+                #markup
+                pg.doubleClick(x=56, y=181)
+                pg.write(f'$C${celula_excel}:$G${celula_excel}')
+                pg.press('enter')
+                pg.hotkey('ctrl', 'c')
+
+                teste = pyperclip.paste()
+
+                num = ''
+                num_list = []
+                final_list = []
+                for c in teste:
+                    if c == ',':
+                        num += '.'
+                    elif c == '\t' or c == '\n':
+                        if c == teste[0]:
+                            continue
+                        else:
+                            num_list.append(num)
+                            num = ''
+                    elif c == '\r':
+                        continue
+                    else:
+                        num += c
+
+                for i, v in enumerate(num_list):
+                    if v != 'Indisponível':
+                        final_list.append(float(v))
+
+                try:
+                    celula = f'{(sum(final_list) / len(final_list)) * 1.8:.2f}'
+                except ZeroDivisionError:
+                    pass
+                else:
+                    pg.doubleClick(x=56, y=181)
+                    pg.write('h' + str(celula_excel))
+                    pg.press('enter')
+                    pyperclip.copy(celula)
+                    pg.hotkey('ctrl', 'v')
+                pg.press('esc')
 
                 #indice increase
                 celula_excel += 1
@@ -931,6 +1014,47 @@ for peça in produtos:
                 pg.hotkey('ctrl', 'v')
                 pg.write('+40')
                 pg.press('enter')
+
+            #markup
+                pg.doubleClick(x=56, y=181)
+                pg.write(f'$C${celula_excel}:$G${celula_excel}')
+                pg.press('enter')
+                pg.hotkey('ctrl', 'c')
+
+                teste = pyperclip.paste()
+
+                num = ''
+                num_list = []
+                final_list = []
+                for c in teste:
+                    if c == ',':
+                        num += '.'
+                    elif c == '\t' or c == '\n':
+                        if c == teste[0]:
+                            continue
+                        else:
+                            num_list.append(num)
+                            num = ''
+                    elif c == '\r':
+                        continue
+                    else:
+                        num += c
+
+                for i, v in enumerate(num_list):
+                    if v != 'Indisponível':
+                        final_list.append(float(v))
+
+                try:
+                    celula = f'{(sum(final_list) / len(final_list)) * 1.8:.2f}'
+                except ZeroDivisionError:
+                    pass
+                else:
+                    pg.doubleClick(x=56, y=181)
+                    pg.write('h' + str(celula_excel))
+                    pg.press('enter')
+                    pyperclip.copy(celula)
+                    pg.hotkey('ctrl', 'v')
+                pg.press('esc')
 
             #indice increase
             celula_excel += 1
