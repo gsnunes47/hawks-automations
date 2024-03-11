c54cbae36ce66e3de64a8125ce183a1179cff2ef
Gustavo Nunes <gustavompn06@gmail.com>
2024-03-11 13:52:14 -0300

    markup test

--- assisted_search.py
+++ assisted_search.py
@@ -261,6 +261,46 @@ else:
     pg.write('+40')
     pg.press('enter')
 
+pg.doubleClick(x=56, y=181)
+pg.write('$C$2:$G$2')
+pg.press('enter')
+pg.hotkey('ctrl', 'c')
+
+teste = pyperclip.paste()
+
+num = ''
+num_list = []
+final_list = []
+for c in teste:
+    if c == ',':
+        num += '.'
+    elif c == '\t' or c == '\n':
+        if c == teste[0]:
+            continue
+        else:
+            num_list.append(num)
+            num = ''
+    elif c == '\r':
+        continue
+    else:
+        num += c
+
+for i, v in enumerate(num_list):
+    if v != 'Indisponível':
+        final_list.append(float(v))
+
+try:
+    celula = f'{(sum(final_list) / len(final_list)) * 1.8:.2f}'
+except ZeroDivisionError:
+    pass
+else:
+    pg.doubleClick(x=56, y=181)
+    pg.write('h2')
+    pg.press('enter')
+    pyperclip.copy(celula)
+    pg.hotkey('ctrl', 'v')
+pg.press('esc')
+
 celula_peca = 2
 for peça in produtos:
     celula_excel = celula_peca
--- new_test.py
+++ new_test.py
@@ -5,39 +5,41 @@ import time
 
 alttab()
 pg.doubleClick(x=56, y=181)
-pg.write('c5')
+pg.write('$C$2:$G$2')
 pg.press('enter')
-# with pg.hold('shift'):
-pg.keyDown('shift')
-time.sleep(5)
-pg.press(['right', 'right', 'right', 'right', 'right'])
-pg.keyUp('shift')
 pg.hotkey('ctrl', 'c')
 
 teste = pyperclip.paste()
-print(teste)
-# num = ''
-# num_list = []
-# final_list = []
-# for c in teste:
-#     if c == ',':
-#         num += '.'
-#     elif c == '\t' or c == '\n':
-#         if c == teste[0]:
-#             continue
-#         else:
-#             num_list.append(num)
-#             num = ''
-#     elif c == '\r':
-#         continue
-#     else:
-#         num += c
 
-# # print(num_list)
+num = ''
+num_list = []
+final_list = []
+for c in teste:
+    if c == ',':
+        num += '.'
+    elif c == '\t' or c == '\n':
+        if c == teste[0]:
+            continue
+        else:
+            num_list.append(num)
+            num = ''
+    elif c == '\r':
+        continue
+    else:
+        num += c
 
-# for i, v in enumerate(num_list):
-#     if v != 'Indisponível':
-#         final_list.append(float(v))
+for i, v in enumerate(num_list):
+    if v != 'Indisponível':
+        final_list.append(float(v))
 
-# celula = f'{(sum(final_list) / len(final_list)) * 1.8:.2f}'
-# print(celula)
+try:
+    celula = f'{(sum(final_list) / len(final_list)) * 1.8:.2f}'
+except ZeroDivisionError:
+    pass
+else:
+    pg.doubleClick(x=56, y=181)
+    pg.write('h2')
+    pg.press('enter')
+    pyperclip.copy(celula)
+    pg.hotkey('ctrl', 'v')
+pg.press('esc')
